c02bce43bcbd8c2e7861c3f416fd557f3d0e047c
harshaajesh <harshagopinathan@gmail.com>
2022-03-14 16:12:27 +1000

    So,ution Created

---
+++ Competition Task/Competition Task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Competition_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
